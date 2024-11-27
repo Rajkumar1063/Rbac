f42eac845f4ec179944742db5cef1ecb4f5eb593
Rajkumar1063 <rajkumarssgsr@gmail.com>
2024-11-28 00:51:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './Components/LoginPage';
 import DataAnalyst from './Components/DataAnalyst';
 import ComplianceOfficer from './Components/ComplianceOfficer';
